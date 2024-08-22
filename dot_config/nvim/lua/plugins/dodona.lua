cf8cfc77aca273bf1be0472e1c877b0486bd568f
Tristan Degraeve <tristan.degraeve@gmail.com>
2024-08-22 21:16:52 +0200

    fixed DodonaInitCourseActivities not available

--- dot_config/nvim/lua/plugins/dodona.lua
+++ dot_config/nvim/lua/plugins/dodona.lua
@@ -1,18 +1,18 @@
 return {
-  "tst-038/dodona.nvim",
-  requires = {
-    "rcarriga/nvim-notify",
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-  },
+	"tst-038/dodona.nvim",
+	requires = {
+		"rcarriga/nvim-notify",
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+	},
 
-  config = function()
-    require("dodona").setup {
-      base_url = "https://dodona.be",
-      go_cmd = "open",
-      use_folders = true,
-    }
-  end,
+	config = function()
+		require("dodona").setup({
+			base_url = "https://dodona.be",
+			go_cmd = "open",
+			use_folders = true,
+		})
+	end,
 
-  cmd = { "DodonaSubmit", "DodonaInitActivities", "DodonaDownload", "DodonaGo" },
+	cmd = { "DodonaSubmit", "DodonaInitActivities", "DodonaInitCourseActivities", "DodonaDownload", "DodonaGo" },
 }
